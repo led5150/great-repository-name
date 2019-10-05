606e8561ad5e9b8ff26417a1ff8ef96ab65113b1
Matt Champlin <56164963+led5150@users.noreply.github.com>
2019-10-04 21:17:51 -0400

    Update genRandCommandFile.cpp

--- genRandCommandFile.cpp
+++ genRandCommandFile.cpp
@@ -12,6 +12,10 @@
  *                   output was uneven, now it is based on the number of
  *                   passengers to be created, which creates a more even
  *                   distribution.
+ *         10/4/19 - Added feature - If user runs with 0 or Negative 
+ *                   passengers to be created, user is given option to 
+ *                   run in Move Metro Only Mode. They also have the 
+ *                   option to quit, and the program will "explode" :)
  ****************************************************************************/
 
 #include <iostream>
@@ -19,6 +23,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string>
+#include <unistd.h>
 
 using namespace std;
 
@@ -38,9 +43,11 @@ const string NEWFILE       = "outB.txt"; // If USEFILENAME is true, this is the
                                          // name of the new file created with 
                                          // the "f [filename]" command.
 bool   initialize(int argc, char *argv[], ofstream &out, int *numptr);
-void   genFile(ofstream &out, int numPass);
+bool   genFile(ofstream &out, int numPass);
 void   exitMessage(char *argv[]);
-void   setParameters(int *FCP, int *NM, int *IEM,  int *NP);
+void   errorMessage();
+bool   setParameters(int *FCP, int *NM, int *IEM, int *NP);
+bool   moveMetroMode(int *IEM);
 string createRandPassenger();
 
 
@@ -57,7 +64,10 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     else {
-        genFile(commandFile, numPass);
+        if (! genFile(commandFile, numPass)) {
+            errorMessage();
+            return 0;
+        }
     }
     exitMessage(argv);
     return 0;
@@ -109,20 +119,23 @@ bool initialize(int argc, char *argv[], ofstream &out, int *numptr) {
 //             by the "m m" (move metro) command for as many times as given
 //             by the user in the command line (numPass).  Always prints the
 //             "m f" command as the last output.  If USEFFILENAME is true,
-//             the "f [filename]" command will be inserted at 70% 
-//             of the total number of passengers given by user to ensure that
-//             we get a nice even distribution of passengers to both 
-//             output files.
-void genFile(ofstream &out, int numPass) {
+//             the "f [filename]" command will be inserted at varying 
+//             percentages of the total number of passengers given by user.
+//             This is to ensure that we get a nice even distribution 
+//             of passengers to both output files.         
+bool genFile(ofstream &out, int numPass) {
     int fileCommandPosition = 0;
     int numMoves = 1;
     int insertExtraMoves = 0;
+
     int *FCP = &fileCommandPosition;
     int *NM  = &numMoves;
     int *IEM = &insertExtraMoves;
     int *NP  = &numPass;
 
-    setParameters(FCP, NM, IEM, NP);
+    if (! setParameters(FCP, NM, IEM, NP)) {
+        return false;
+    }
 
     for (int i = 0; i < numPass; i++) {
         out << createRandPassenger() << endl;
@@ -134,10 +147,10 @@ void genFile(ofstream &out, int numPass) {
         }
     }
     if (insertExtraMoves > 0) {
-        if (USEFFILENAME) {
-            for (int i = 0; i < insertExtraMoves; i++) {
-                    out << MOVEMETRO << endl;
-                }
+        for (int i = 0; i < insertExtraMoves; i++) {
+            out << MOVEMETRO << endl;
+        }
+        if (USEFFILENAME) {    
             out << "f " << NEWFILE << endl;
         }
     }
@@ -146,6 +159,7 @@ void genFile(ofstream &out, int numPass) {
         out << MOVEMETRO << endl;
     }
     out << METROFINISH << endl;
+    return true;
 }
 
 // Function:   setParameters *** Helper Function for genFile ***
@@ -159,23 +173,73 @@ void genFile(ofstream &out, int numPass) {
 //             small test sizes were asked for, which resulted in uneven
 //             distribution of passengers to the two files.  Now the 
 //             distribution is based on the number of passengers.
-void setParameters(int *FCP, int *NM, int *IEM, int *NP) {
-    if (*NP < 25) {
-        *NM = *NM + 3;
-        *IEM = *NP + (*NP * .7);
+bool setParameters(int *FCP, int *NM, int *IEM, int *NP) {
+    // Catch exception if user inputs 0 or a negative number for numPassengers
+    // Allows user an "out" to test in Move Metro Only Mode.
+    if (*NP <= 0) {
+        if (! moveMetroMode(IEM)) {
+            return false;
+        }
     }
-    if (*NP >= 25 and *NP < 50) {
-        *NM = *NM + 2;
-        *FCP = *NP - 1;
+    if (*NP >=1 and *NP <= 10) {
+        *NM  = *NM + 5;
+        *IEM = NUMSTATIONS;
     }
-    if (*NP >= 50 and *NP < 100) {
-        *NM = *NM + 1.5;
+    else if (*NP > 10 and *NP <= 24) {
+        *NM  = *NM + 3;
+        *IEM = *NP + (*NP * .6);
+    }
+    else if (*NP >= 25 and *NP < 50) {
+        *NM  = *NM + 2;
+        *FCP = *NP * .5;
+        *IEM = NUMSTATIONS;
+    }
+    else if (*NP >= 50 and *NP < 100) {
+        *NM  = *NM + 1.5;
         *FCP = *NP * .9;
     }
-    if (*NP >= 100) {
-        *NM = *NM + 1;
+    else if (*NP >= 100) {
+        *NM  = *NM + .1;
         *FCP = *NP * .8;
     }
+    return true;
+}
+
+// Function:   moveMetroMode
+// Parameters: int *IEM - A pointer to the insertExtraMoves int
+// Returns:    True if user wants to run in Move Metro Only Mode, False if
+//             not or there is bad input from user 
+// Does:       Runs program in Move Metro Only Mode!  Will create a file
+//             of user specified length with only the move metro command.
+bool moveMetroMode(int *IEM) {
+    string input;
+    cout << "No Passengers to be created...Did you want to test in " 
+            "Move Metro Only Mode?" << endl;
+    cout << "type 'y' for yes and I'll insert a bunch of move metro "
+            "commands" << endl;
+    cout << "Type 'n' for no and to quit." << endl;
+    cin >> input;
+    while (input != "y" or input != "n") {
+        if (input == "y") {
+            int moves = 0;
+            cout << "How many Move Metro commands would you like?" << endl;
+            cin >> moves;
+            if (cin.fail()) {
+                cerr << "That's not a number!!" << endl;
+                return false;
+            }
+            *IEM = moves;
+            return true;
+        }
+        else if (input == "n") {
+            return false;
+        }
+        else {
+            cerr << "Bad Input, try again. 'y' or 'n' please" << endl;
+            cin >> input;
+        }
+    }
+    return true;
 }
 
 // Function:   createRandPassenger
@@ -208,7 +272,24 @@ void exitMessage(char *argv[]) {
     cout << ": ) : )       Happy Train Testing!    : ) : )" << endl;
 }
 
-
+// Function:   errorMessage
+// Parameters: None
+// Returns:    Nothing
+// Does:       Prints an error message to cerr if command file not able to 
+//             be created for any reason.
+void errorMessage() {
+    cerr << "Command File was not able to be succesfully created..." << endl;
+             cerr << "Possibly due to bad input, too few passengers or "
+                     "some other (user) error. :( :( :(" << endl;
+             cerr << "Get it right next time! Program exploding in..." << endl;
+             cerr << "3" << endl;
+             usleep(900000);
+             cerr << "2" << endl;
+             usleep(900000);
+             cerr << "1" << endl;
+             usleep(900000);
+             cerr << "BOOM!" << endl;
+}
 
 
 
