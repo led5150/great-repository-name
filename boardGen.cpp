80751d29b2f6b4eb3ed0e15da60ce8ac88be3f61
Matt Champlin <56164963+led5150@users.noreply.github.com>
2019-10-20 02:14:37 -0400

    Update boardGen.cpp

--- boardGen.cpp
+++ boardGen.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <time.h>
 
 using namespace std;
@@ -20,23 +21,33 @@ const int size = 15;
 
 void makeDefaultBoard(char board[size][size]);
 void insertWalls(char board[size][size]);
-bool printToFile(char board[size][size], char *argv[]);
+bool printToFile(vector <char> addSpace, char *argv[]);
 void success(char *argv[]);
-void tryagain(char board[size][size], char *argv[]);
-void printBoard(char board[size][size]);
+void tryagain(vector <char> addSpace, char board[size][size], char *argv[]);
+void printBoard(vector <char> addSpace);
+vector<char> insertSpaces(char board[size][size]);
+
+
 
 
 int main(int argc, char *argv[]) {
 
     // initialize random seed
     srand(time(NULL));
+
     // create 15 x 15 char array
     char board[size][size];
 
+    // initialize a vector to be able to add spaces in the correct places
+    vector <char> addSpace;
+
+    // Generate that board!
     if (argc == 2) {
         makeDefaultBoard(board);
         insertWalls(board);
-        if (printToFile(board, argv)) {
+        addSpace = insertSpaces(board);
+
+        if (printToFile(addSpace, argv)) {
             success(argv);
         }
         else {
@@ -48,12 +59,38 @@ int main(int argc, char *argv[]) {
         cerr << "Try again!" << endl;
         return 0;
     }
-    
-    tryagain(board, argv);
+    // Allow the user to see the board generated and decide whether or not to
+    // keep it!  Some boards are inherently 'boring' and not useful for testing
+    // unless the test is to get a No Solution outcome!  This option allows 
+    // user to choose.
+    tryagain(addSpace, board, argv);
 
     return 0;
 }
 
+// Function:   insertSpaces
+// Parameters: char board - a board of demensions size x size
+// Returns:    A vector with the board information plus spaces inserted
+//             to ensure proper output format
+// Does:       Initializes a temp vector, takes the board information from the
+//             2d char board and inserts spaces after each board character to
+//             ensure the proper format when printing to file.
+vector<char> insertSpaces(char board[size][size]) 
+{
+    vector <char> temp;
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            temp.push_back(board[i][j]);
+            if (j < size - 1){
+                temp.push_back(' ');
+            }
+            
+        }
+    }
+    return temp;
+}
 
 // Function:   makeDefaultBoard
 // Parameters: char board - a board of demensions size x size
@@ -111,12 +148,13 @@ void insertWalls(char board[size][size]) {
 }
 
 // Function:   printToFile
-// Parameters: char board   - a board of demensions size x size
-//             char *argv[] - The filename user specifies
+// Parameters: vector addSpace - A vector with proper format 
+//             char *argv[]    - The filename user specifies
 // Returns:    True if board was succesfully created, False if not
 // Does:       Prints the board to the file the user specifies in the command
 //             line.
-bool printToFile(char board[size][size], char *argv[]) {
+bool printToFile(vector <char> addSpace, char *argv[]) 
+{
 
     ofstream out;
     out.open(argv[1]);
@@ -125,13 +163,11 @@ bool printToFile(char board[size][size], char *argv[]) {
         return false;
     }
     else {
-        for (int i = 0; i < size; ++i) 
-        {
-            for (int j = 0; j < size; ++j)
-            {
-                out << board[i][j];
+        for (unsigned int i = 0; i < addSpace.size(); i++) {
+            out << addSpace[i];
+            if ((i + 1)  % 29 == 0) {
+                out << endl;
             }
-            out << endl;
         }
     }
     return true;
@@ -140,6 +176,7 @@ bool printToFile(char board[size][size], char *argv[]) {
 
 // Function:   tryagain
 // Parameters: char board   - a board of demensions size x size
+//             vector addSpace - A vector with proper format
 //             char *argv[] - The filename user specifies
 // Returns:    Nothing
 // Does:       Allows the user to see the board that was just created on cout
@@ -147,9 +184,10 @@ bool printToFile(char board[size][size], char *argv[]) {
 //             If the user is unhappy, they can re-make the board with 
 //             new random walls.  Does so recursively within the function itelf
 //             until user is happy and types n
-void tryagain(char board[size][size], char *argv[]) {
+void tryagain(vector <char> addSpace, char board[size][size], char *argv[]) 
+{
     cout << "Your board looks like: \n\n"; 
-    printBoard(board);
+    printBoard(addSpace);
     char input = ' ';
     cout << "Would you like to try again? 'y' or 'n' " << endl;
     cin >> input;
@@ -160,22 +198,23 @@ void tryagain(char board[size][size], char *argv[]) {
         }
         if (input == 'y')
         {
+            addSpace.clear();
             makeDefaultBoard(board);
             insertWalls(board);
-            if (printToFile(board, argv)) {
+            addSpace = insertSpaces(board);
+            if (printToFile(addSpace, argv)) {
                 success(argv);
             }
             else {
                 cerr << "Board unable to be created" << endl;
                 return;
             }
-            tryagain(board, argv);
+            tryagain(addSpace, board, argv);
         }
         else {
             cout << "************** FILE NOT UPDATED *************" << endl;
             cout << "Please type 'y' or 'n'!!" << endl << endl;
-            // input.clear();
-            tryagain(board, argv);
+            tryagain(addSpace, board, argv);
         }
         return;
         
@@ -183,16 +222,26 @@ void tryagain(char board[size][size], char *argv[]) {
     return;
 }
 
-void printBoard(char board[size][size]) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j)
-        {
-            cout << board[i][j];
+// Function:   printBoard
+// Parameters: vector addSpace - A vector with proper format
+// Returns:    Nothing
+// Does:       Prints the board to cout so user can see if they are happy with
+//             it
+void printBoard(vector <char> addSpace) 
+{
+    for (unsigned int i = 0; i < addSpace.size(); i++) {
+            cout << addSpace[i];
+            if ((i + 1)  % 29 == 0) {
+                cout << endl;
+            }
         }
-        cout << endl;
-    }
 }
 
+
+// Function:   success
+// Parameters: char argv - the filename specified by user
+// Returns:    Nothing
+// Does:       Prints to cout if board is successfully created
 void success(char *argv[]) {
     cout << "Your random board has been generated as " << argv[1] << " !\n";
     cout << "Lets GO Robots!" << endl;
